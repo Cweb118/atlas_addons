1274af974ea500d0eb5354dc43d90f35d742605a
Cweb118 <cweb1182@gmail.com>
2022-08-17 14:59:13 -0400

    added logging

--- atlas_addons/templates/template_run_all_parallel.py
+++ atlas_addons/templates/template_run_all_parallel.py
@@ -1,19 +1,33 @@
 import os
 import asyncio
+from datetime import datetime
 
 async def run_cmd(cmd):
     try:
+        log_name = cmd.replace('.py', '_log.txt')
+        logtxt = cmd+" start time: "+str(datetime.now())+'\n'
+        logfile = open(log_name, "w")
+
         proc = await asyncio.create_subprocess_shell(
             "python "+cmd,
             shell=True,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE)
         stdout, stderr = await proc.communicate()
-        print(f'[{cmd!r} exited with {proc.returncode}]')
+        msg = f'[{cmd!r} exited with {proc.returncode}]'
+        print(msg)
+        logtxt += msg+'\n'
         if stdout:
-            print(f'[stdout]\n{stdout.decode()}')
+            msg = f'[stdout]\n{stdout.decode()}'
+            print(msg)
+            logtxt += msg+'\n'
         if stderr:
-            print(f'[stderr]\n{stderr.decode()}')
+            msg = f'[stderr]\n{stderr.decode()}'
+            print(msg)
+            logtxt += msg+'\n'
+        logtxt += cmd+" end time: "+str(datetime.now())
+        logfile.write(logtxt)
+        logfile.close()
     except asyncio.CancelledError:
         proc.terminate()
         print("Process Terminated")
